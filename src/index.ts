af7b532aabcea3bf603234667a7a5668b9743208
Dmitrii Shulaikin <dmitrii@shulaikin.tech>
2021-01-29 16:48:09 +0300

    polish library

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-export { Card } from './components/Card'
-export { Checkbox } from './components/Checkbox'
-export { Select } from './components/Select'
+import styled from 'styled-components';
+export { styled };
+
 export { Button } from './components/Button'
-export { Text } from './components/Text'
+export { Card } from './components/Card'
+export { Centered } from './components/Centered';
+export { Checkbox } from './components/Checkbox'
+export { Chip } from './components/Chip';
+export { FormItem } from './components/FormItem';
+export { Icon } from './components/Icon';
+export { List } from './components/List';
+export { ListItem } from './components/ListItem';
+export { Loader } from './components/Loader';
+export { RoundedImage } from './components/RoundedImage';
+export { Select } from './components/Select';
+export { Text } from './components/Text';
+export { TextInput } from './components/TextInput';
 export { Theme } from './themes/theme'
+
 export { defaultTheme } from './themes/defaultTheme'
 export { defaultDarkTheme } from './themes/defaultDarkTheme'
+
 export { ThemeProvider } from 'styled-components'
